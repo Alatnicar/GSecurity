5ef656ef7dd6792baa889eafc8bb11fafe5d3753
Alatnicar <znastidobrostoje@gmail.com>
2025-06-28 06:22:11 -0700

    Add files via upload

---
+++ Iso/sources/$OEM$/$$/Setup/Scripts/Bin/GSecurity.cmd
@@ -0,0 +1,16 @@
+:: Perms
+icacls "%systemdrive%\Users" /remove "Everyone"
+icacls "%systemdrive%\Users\Public" /reset
+icacls "%systemdrive%\Users\Public" /inheritance:r
+icacls "%systemdrive%\Users\Public" /setowner "console logon" /t
+icacls "%USERPROFILE%\" /setowner "%username%" /t /c /l
+icacls "%USERPROFILE%\" /remove "System" /t /c /l
+icacls "%USERPROFILE%\" /remove "Administrators" /t /c /l
+
+:: Services stop and disable
+sc stop LanmanWorkstation
+sc stop LanmanServer
+sc stop seclogon
+sc config LanmanWorkstation start= disabled
+sc config LanmanServer start= disabled
+sc config seclogon start= disabled
